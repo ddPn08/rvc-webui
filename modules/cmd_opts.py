5854ce15124cf2a9a9e0fe24040310e8646d3ecf
ddPn08 <pyn.g.1125@gmail.com>
2023-04-13 10:10:29 +0900

    fix default port

--- modules/cmd_opts.py
+++ modules/cmd_opts.py
@@ -3,7 +3,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--host", help="Host to connect to", type=str, default="localhost")
-parser.add_argument("--port", help="Port to connect to", type=int, default=7680)
+parser.add_argument("--port", help="Port to connect to", type=int)
 parser.add_argument("--share", help="Enable gradio share", action="store_true")
 parser.add_argument(
     "--models-dir", help="Path to models directory", type=str, default=None
--- webui.py
+++ webui.py
@@ -1,14 +1,15 @@
 from modules import ui, cmd_opts
-import faiss
 
 
 def webui():
     app = ui.create_ui()
-    app, local_url, share_url = app.queue(concurrency_count=511, max_size=1022).launch(
+    app.queue(64)
+    app, local_url, share_url = app.launch(
         server_name=cmd_opts.opts.host,
         server_port=cmd_opts.opts.port,
         share=cmd_opts.opts.share,
     )
+
 
 if __name__ == "__main__":
     webui()
