c3f05f23af41c138582f657458b17d5244d29c5a
ddPn08 <pyn.g.1125@gmail.com>
2023-04-13 21:31:40 +0900

    improve update script

--- update.bat
+++ update.bat
@@ -1,4 +1,10 @@
 @echo off
+if exist .git goto :reset
+
+git init
+git remote add origin https://github.com/ddPn08/rvc-webui.git
+
+:reset
 git fetch --prune
 git reset --hard origin/main
 pause
--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-git fetch --prune
-git reset --hard origin/main
+if [ -d .git ]; then
+    git fetch --prune
+    git reset --hard origin/main
+else
+    git init
+    git remote add origin
+    git fetch --prune
+    git reset --hard origin/main
