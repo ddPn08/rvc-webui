9fa1353cffdd7d2992132a49b4efb0541066d837
ddPn08 <pyn.g.1125@gmail.com>
2023-04-14 10:20:53 +0900

    mps support | WIP

--- modules/shared.py
+++ modules/shared.py
@@ -1,15 +1,39 @@
 import os
+import sys
 
 import torch
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODELS_DIR = os.path.join(ROOT_DIR, "models")
 
-is_half = False
+
+def has_mps() -> bool:
+    if sys.platform != "darwin":
+        return False
+    else:
+        if not getattr(torch, "has_mps", False):
+            return False
+        try:
+            torch.zeros(1).to(torch.device("mps"))
+            return True
+        except Exception:
+            return False
+
+
+is_half = True
 
 device = "cuda:0"
 
-if device != "cpu":
+if not torch.cuda.is_available():
+    if has_mps():
+        print("Using MPS")
+        device = "mps"
+    else:
+        print("Using CPU")
+        device = "cpu"
+        is_half = False
+
+if device not in ["cpu", "mps"]:
     gpu_name = torch.cuda.get_device_name(int(device.split(":")[-1]))
     if "16" in gpu_name or "MX" in gpu_name:
         is_half = False
